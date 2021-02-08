14189a37c3d10b116691da07d748313c9d69baeb
EhsanRP <sir.ehrp@gmail.com>
2021-02-08 23:50:07 +0330

    Implemented Security Configurations

---
+++ src/main/java/com/equadesign/loginPage/security/SecurityConfig.java
@@ -0,0 +1,56 @@
+package com.equadesign.loginPage.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                .csrf().ignoringAntMatchers("/h2-console/**").and()
+                .authorizeRequests(authorize -> {
+                    authorize
+                            .mvcMatchers("/h2-console/**").permitAll()
+                            .mvcMatchers("/", "/webjars/**", "/resources/**").permitAll()
+                            .mvcMatchers("/login/**", "/register/**").permitAll();
+                })
+                .authorizeRequests()
+                .anyRequest().authenticated();
+
+        http
+                .formLogin()
+                .loginPage("/login.html")
+                .loginProcessingUrl("/perform_login")
+                .defaultSuccessUrl("/homepage.html", true)
+                .failureUrl("/login.html?error=true")
+                .and()
+                .logout()
+                .logoutUrl("/perform_logout")
+                .deleteCookies("JSESSIONID");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
